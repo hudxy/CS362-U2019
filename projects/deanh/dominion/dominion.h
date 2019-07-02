09df611182807fe27a173f4aba723d146357f809
hudxy <45858521+hudxy@users.noreply.github.com>
2019-07-02 14:18:09 -0700

    Refactor and Implement Bugs
    Assignment 2 requirements to refactor functions and implement 2 bugs for each function

--- projects/deanh/dominion/dominion.h
+++ projects/deanh/dominion/dominion.h
@@ -85,7 +85,7 @@ int initializeGame(int numPlayers, int kingdomCards[10], int randomSeed,
 		   struct gameState *state);
 /* Responsible for initializing all supplies, and shuffling deck and
    drawing starting hands for all players.  Check that 10 cards selected
-   are in fact (different) kingdom cards, and that numPlayers is valid. 
+   are in fact (different) kingdom cards, and that numPlayers is valid.
 
 Cards not in game should initialize supply position to -1 */
 
@@ -127,5 +127,23 @@ int scoreFor(int player, struct gameState *state);
 int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
+
+void baron(struct gameState *state, int currentPlayer, int choice1);
+/* Function for baron card in cardEffect Function */
+
+void minion(struct gameState *state, int currentPlayer, int choice1, int choice2);
+/* Function for minion card in cardEffect Function */
+
+int ambassador(struct gameState *state, int currentPlayer, int choice1, int choice2);
+/* Function for ambassador card in cardEffect Function */
+
+void tribute(struct gameState *state, int currentPlayer, int tributeRevealedCards[]);
+/* Function for tribute card in cardEffect Function */
+
+int mine(struct gameState *state, int currentPlayer, int choice1, int choice2);
+/* Function for mine card in cardEffect Function */
+
+int handCountSlide(struct gameState *state, int currentPlayer, int interator);
+/* Helper Function that slides the players hand down to remove a card from the players hand */
 
 #endif
